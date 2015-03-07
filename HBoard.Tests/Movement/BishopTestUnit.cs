c41c34249d406352cf46ac85ef213db99e33c989
Hyftar <Hyftar@users.noreply.github.com>
2015-03-07 02:01:55 -0500

    Rename HChess to HBoard

--- HBoard.Tests/Movement/BishopTestUnit.cs
+++ HBoard.Tests/Movement/BishopTestUnit.cs
@@ -10,10 +10,10 @@ using HBoard.Chess.Generation;
 using HBoard.Chess.Units;
 using HBoard.Logic;
 
-namespace HChess.Tests.Movement
+namespace HBoard.Tests.Movement
 {
     [TestClass]
-    public class RookTestUnit
+    public class BishopTestUnit
     {
         [TestMethod]
         public void TestClassicDefault()
@@ -31,19 +31,19 @@ namespace HChess.Tests.Movement
             });
             gameContext.Init();
 
-            var rook = gameContext.Board
-                .Select((x, i) => new { Index = i, Cell = x == null ? null : x.Content as RookUnit })
+            var bishop = gameContext.Board
+                .Select((x, i) => new { Index = i, Cell = x == null ? null : x.Content as BishopUnit})
                 .Where(x => x.Cell != null)
                 .First();
 
-            var path = rook.Cell.GetMovementPaths(gameContext.Board, new Point(rook.Index / 8, rook.Index % 8)).First();
+            var path = bishop.Cell.GetMovementPaths(gameContext.Board, new Point(bishop.Index / 8, bishop.Index % 8)).First();
 
             Assert.AreEqual(0, path.First().Length);
             Assert.AreEqual(0, path.Last().Length);
         }
 
         [TestMethod]
-        public void TestAxes()
+        public void TestLastAxisIntersectOrigin()
         {
             var options = new GameOptions
             {
@@ -52,9 +52,9 @@ namespace HChess.Tests.Movement
                     ChessPlayer white = (ChessPlayer) context.Players.First(),
                                 black = (ChessPlayer) context.Players.Last();
 
-                    board.AddUnit<RookUnit>(Orientation.Horizontal, white, Point.Empty, new Point(3, 4));
-                    board.AddUnit<BishopUnit>(Orientation.Horizontal, white, Point.Empty, new Point(2, 4));
-                    board.AddUnit<PawnUnit>(Orientation.Horizontal, black, Point.Empty, new Point(4, 6));
+                    board.AddUnit<BishopUnit>(Orientation.Vertical, white, Point.Empty, new Point(3, 4));
+                    board.AddUnit<RookUnit>(Orientation.Vertical, white, Point.Empty, new Point(1, 2));
+                    board.AddUnit<PawnUnit>(Orientation.Vertical, black, Point.Empty, new Point(5, 1));
                 },
                 BoardSize = new Size(8, 8)
             };
@@ -66,23 +66,23 @@ namespace HChess.Tests.Movement
             });
             gameContext.Init();
 
-            var rook = gameContext.Board
-                .Select((x, i) => new { Index = i, Cell = x == null ? null : x.Content as RookUnit })
+            var bishop = gameContext.Board
+                .Select((x, i) => new { Index = i, Cell = x == null ? null : x.Content as BishopUnit })
                 .Where(x => x.Cell != null)
                 .First();
 
-            var paths = rook.Cell.GetMovementPaths(gameContext.Board, new Point(rook.Index / 8, rook.Index % 8));
-            MovementPath horizontalPath = paths.First(), verticalPath = paths.Last();
+            var paths = bishop.Cell.GetMovementPaths(gameContext.Board, new Point(bishop.Index / 8, bishop.Index % 8));
+            MovementPath primePath = paths.First(), lastPath = paths.Last();
 
-            // Horizontal axis
-            Assert.AreEqual(horizontalPath.First().Direction, Direction.Horizontal);
-            Assert.AreEqual(6, horizontalPath.First().Length);
-            Assert.AreEqual(new Point(3, 0), horizontalPath.Location);
+            // Prime axis
+            Assert.AreEqual(primePath.First().Direction, Direction.Diagonal);
+            Assert.AreEqual(3, primePath.First().Length);
+            Assert.AreEqual(new Point(2, 3), primePath.Location);
 
-            // Vertical axis
-            Assert.AreEqual(verticalPath.First().Direction, Direction.Vertical);
-            Assert.AreEqual(4, verticalPath.First().Length);
-            Assert.AreEqual(new Point(4, 4), verticalPath.Location);
+            // Last axis
+            Assert.AreEqual(lastPath.First().Direction, Direction.Diagonal);
+            Assert.AreEqual(7, lastPath.First().Length);
+            Assert.AreEqual(new Point(0, 7), lastPath.Location);
         }
     }
 }
